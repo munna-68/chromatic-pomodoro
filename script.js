12b4936e5ff77b9a709bc93c81cf1d366386f531
munna-68 <mahamudmunna68@gmail.com>
2026-03-02 13:14:02 +0600

    staring on the actual funtionility of the app

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('working');
+const settingModalOverlay = document.getElementById("settings-modal");
+const closeBtn = document.getElementById("close-btn");
+const settingsBtn = document.getElementById("settings-btn");
+
+console.log("working");
+
+const UIChanges = {
+  closeSettingsModal(e) {
+    settingModalOverlay.classList.add("hidden");
+  },
+  openSettingModal(e) {
+    if (settingModalOverlay.classList.contains("hidden"))
+      settingModalOverlay.classList.remove("hidden");
+  },
+};
+
+if (closeBtn) {
+  closeBtn.addEventListener("click", UIChanges.closeSettingsModal);
+}
+
+if (settingsBtn) {
+  settingsBtn.addEventListener("click", UIChanges.openSettingModal);
+}
